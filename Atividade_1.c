790eb0c88dd4b396f25d6968d9495ead737f36a3
SNesht <101153084+SNesht@users.noreply.github.com>
2023-03-22 00:22:25 -0300

    1.5a contribuição

---
+++ Atividade_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Nestor Felipe Borges do Carmo\n");
+    printf("Aluno do curso de Engenharia da Computação\n");
+    printf("matricula 202206840052\n");
+
+    return 0;
+}
