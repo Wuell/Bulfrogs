8b2882187b8a45ca1a37cfdaa4db472c5306e99f
Wuell <wellerson.veloso10@gmail.com>
2023-04-30 06:25:49 -0300

    main

---
+++ Main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "Matrizes.h"
+
+int main()
+{
+    printf("=====Equipe====\n\nWellerson Nascimento Matricula = 202206840045\n\nNestor Borges Matricula = 202206840052\n\n");
+
+    teste_hermitiana();
+
+return (0);
+}
