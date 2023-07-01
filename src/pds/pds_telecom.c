eb97ea8b2760765a6f0637eb5d53162da2dee593
Wuell <wellerson.veloso10@gmail.com>
2023-06-30 22:28:13 -0300

    Adição TX_QAM_MAPPER, Atualização makefile

--- src/pds/pds_telecom.c
+++ src/pds/pds_telecom.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../matrizes/Matrizes.h"
-#include "pds_telecom.h"
+#include "Pds_telecom.h"
+#include <time.h>
 
 
-void main(){
+bfgs_vector tx_qam_mapper(int *bits,int number_bits)
+{
 
+    bfgs_vector complex_vector = vector_alloc(number_bits);
+
+    for (int i = 0; i < number_bits; i ++)
+    {
+        if (bits[i] == 0)
+            vector_change(complex_vector, i ,(complexo){-1,1});
+
+        else if (bits[i] == 1)
+            vector_change(complex_vector, i ,(complexo){-1,-1});
+
+        else if (bits[i] == 2)
+            vector_change(complex_vector, i ,(complexo){1,1});
+
+        else//bits[i] == 3)
+            vector_change(complex_vector, i ,(complexo){1,-1});
+    
+    }
+    return complex_vector;
 }
+
+
---
+++ src/pds/pds_telecom.h
@@ -0,0 +1,6 @@
+#ifndef PDS_TELECOM_H
+#define PDS_TELECOM_H
+
+bfgs_vector tx_qam_mapper(int *bits,int number_bits);
+
+#endif
