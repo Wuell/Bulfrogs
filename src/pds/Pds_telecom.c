6450ed7ae7fec5166afca0a90e23d73809b8f2fe
Wuell <wellerson.veloso10@gmail.com>
2023-07-05 21:27:29 -0300

    Nestor fez besteira

--- src/pds/Pds_telecom.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "../matrizes/Matrizes.h"
-#include "Pds_telecom.h"
-#include <time.h>
-
-void main(){
-
-}
--- src/pds/Pds_telecom.h
+++
@@ -1,4 +0,0 @@
-#ifndef PDS_TELECOM_H
-#define PDS_TELECOM_H
-
-#endif
