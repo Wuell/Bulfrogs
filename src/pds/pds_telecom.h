7a3914b3c495f0a7dd53392c95e6af7ce37c9a84
SNesht <101153084+SNesht@users.noreply.github.com>
2023-07-11 00:54:31 -0300

    ajeitando...

--- src/pds/pds_telecom.h
+++ src/pds/pds_telecom.h
@@ -1,13 +1,13 @@
 #ifndef PDS_TELECOM_H
 #define PDS_TELECOM_H
 
-bfgs_vector tx_qam_mapper(int *bits, int number_bits);
+bfgs_vector tx_qam_mapper(bfgs_int_vector bits);
 
-int *rx_qam_demapper(bfgs_vector complex_vector);
+bfgs_int_vector rx_qam_demapper(bfgs_vector complex_vector);
 
 void verify_nstreams(int Nr, int Nt, int Nstreams);
 
-bfgs_matrix tx_layer_mapper(bfgs_vector Vector_QAM, int Nstreams, int NTransmissions);
+bfgs_matrix tx_layer_mapper(bfgs_vector Vector_QAM, int Nt, int Nstreams);
 
 bfgs_vector random_vector_com(int len, int intervalMod);
 
@@ -26,5 +26,13 @@ bfgs_matrix tx_precoder(bfgs_matrix signal,bfgs_matrix v);
 bfgs_vector rx_layer_demapper(bfgs_matrix complex_matrix);
 
 void testa_a_porra_toda(int number_bits,int Nr,int Nt, int Ntstreams);
+
+bfgs_int_vector tx_data_read(char* name);
+
+void rx_data_write(char* name, bfgs_int_vector objct);
+
+bfgs_vector rx_feq(bfgs_vector s, bfgs_matrix data);
+
+bfgs_matrix rx_combiner(bfgs_matrix signal, bfgs_matrix u);
 
 #endif
